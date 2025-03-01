8166b830bc4ebccf00c041d3449b934d9a233cb1
Wojciech Kotlarski <wojciech.kotlarski@posteo.net>
2025-03-01 17:43:26 +0100

    fixed plot rasterization

--- data/plot_mA_tanb.py
+++ data/plot_mA_tanb.py
@@ -3,6 +3,7 @@ import csv
 from numpy import genfromtxt
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.axis import Axis
 
 plt.rcParams["figure.figsize"] = (4,3)
 
@@ -16,10 +17,14 @@ mask = (data[:, 2] > 172.57 - 2*0.29) & (data[:, 2] < 172.57 + 2*0.29)
 data = data[mask, :]
 
 plt.rcParams['text.usetex'] = True
-plt.scatter(data[:,5], data[:, 4], color='limegreen', s=1)
-plt.xlabel(r'$m_A$ [GeV]')
-plt.ylabel(r'$\tan \beta$')
-plt.plot(1000, 1.85,'*',color='red',markersize=10)
 
-plt.savefig('img/mA_vs_tanb.png', bbox_inches='tight', format='png', dpi=2000, pad_inches=0)
+fig, ax = plt.subplots()
+
+m = ax.scatter(data[:,5], data[:, 4], color='limegreen', s=1)
+ax.set_xlabel(r'$m_A$ [GeV]')
+ax.set_ylabel(r'$\tan \beta$')
+ax.plot(1000, 1.85,'*',color='red',markersize=10)
+Axis.set_rasterized(m, True)
+
+plt.savefig('img/mA_vs_tanb.pdf', bbox_inches='tight', format='pdf', dpi=500, pad_inches=0)
 
